dd2b9fc651b4c28cce6a98cbc4f01b7ca9a948e4
Mehmet Akif Bektaş <mehmet.bektas@mebitech.com>
2017-01-28 21:58:08 +0200

    initial commit

---
+++ src/main/java/org/primefaces/pf/calendar/IndexBean.java
@@ -0,0 +1,36 @@
+package org.primefaces.pf.calendar;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Mehmet Akif Bektaş
+ */
+@ManagedBean(name = "index")
+@ViewScoped
+public class IndexBean {
+
+	private Date date;
+
+  public IndexBean() {
+    System.out.println("IndexBean is initialized.");
+    date = Calendar.getInstance().getTime();
+  }
+
+  public String onSelect(){
+    System.out.println("IndexBean - onSelect is called.");
+    System.out.println("Selected Date: " + date);
+    return "index";
+  }
+
+  public Date getDate() {
+	return date;
+}
+  public void setDate(Date date) {
+	this.date = date;
+}
+}
